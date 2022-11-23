84c7d39381726a89df2326369865254b2b31ab24
garden <gkehd1024@gmail.com>
2022-11-23 20:39:53 +0900

    약 추가하기 UI(1)

--- lib/pages/add_medicine/add_medicine_page.dart
+++ lib/pages/add_medicine/add_medicine_page.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddMedicinePage extends StatelessWidget{
@@ -8,8 +7,49 @@ class AddMedicinePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(),
-      //test
+      appBar: AppBar(
+        leading: const CloseButton(),
+      ),
+      body: GestureDetector(
+        onTap: (){
+          FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, //좌측정렬
+          children: [
+            Text(
+              '어떤 약이에요?',
+              style: Theme.of(context).textTheme.headline4
+              ),
+             Center(
+               child: CircleAvatar(
+                radius: 40,
+                child: CupertinoButton (
+                  onPressed: () {},
+                  child: const Icon(
+                    CupertinoIcons.photo_camera_solid,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                )
+            ),
+             ),
+            Text(
+              '약이름',
+              style: Theme.of(context).textTheme.subtitle1
+              ),
+            TextFormField(),
+          ],
+          ),
+      ),
+        bottomNavigationBar: SafeArea(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.subtitle1 
+              ),
+            child: Text('다음')
+        ),
     );
   }
 }
